ee1a3d01a0bcfeaa7769e2e079e2f603c35ee87b
ko-da-k <17734711+ko-da-k@users.noreply.github.com>
2024-02-07 20:38:38 +0900

    get table data with jtc timezone

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,23 @@ import "./App.css";
 // sqlite. The path is relative to `tauri::api::path::BaseDirectory::App`.
 const db = await Database.load("sqlite:afk-track.db");
 
+type AfkTable = {
+  id: number,
+  created_at: string,
+  updated_at: string,
+}
+
+// Away from Keyboard
+type Afk = {
+  id: number,
+  createdAt: Date,
+  updatedAt: Date,
+}
+
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+  const [afks, setAfks] = useState<Afk[]>([]);
 
   useEffect(() => {
     // global shortcut
@@ -26,8 +40,20 @@ function App() {
   async function greet() {
     await db.execute( "INSERT into afk DEFAULT VALUES");
     console.log("insert row");
-    const res = await db.select("SELECT * FROM afk");
-    console.log(res);
+    const res: AfkTable[] = await db.select(`
+      SELECT 
+        id, 
+        datetime(created_at, 'localtime') as created_at, 
+        datetime(updated_at, 'localtime') as updated_at
+      FROM afk
+    `);
+    console.log(res)
+    setAfks(res.map((item) => ({
+      id: item.id,
+      createdAt: new Date(item.created_at),
+      updatedAt: new Date(item.updated_at)
+    })));
+    console.log(afks);
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name }));
   }
@@ -66,6 +92,24 @@ function App() {
       </form>
 
       <p>{greetMsg}</p>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>CreatedAt</th>
+            <th>UpdatedAt</th>
+          </tr>
+        </thead>
+        <tbody>
+          {afks.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.createdAt.toLocaleString()}</td>
+              <td>{item.updatedAt.toLocaleString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
