8bd7655e247e8518d886590e80bf711a2de4d898
Kodai Kato <17734711+ko-da-k@users.noreply.github.com>
2024-02-09 08:18:43 +0900

    sqlite integration (#1)
    * add sqlite
    
    * check globalShortcut
    
    * debug mode
    
    * get table data with jtc timezone
    
    * insert db

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+use tauri_plugin_sql::{Migration, MigrationKind};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,7 +11,42 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let migrations = vec![Migration {
+        version: 1,
+        description: "create_initial_Tables",
+        sql: "
+            CREATE TABLE afk (
+                id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                created_at datetime default current_timestamp,
+                updated_at datetime default current_timestamp
+            );
+            CREATE TABLE bak (
+                id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                created_at datetime default current_timestamp,
+                updated_at datetime default current_timestamp,
+                afk_id INTEGER NOT NULL,
+                FOREIGN KEY(afk_id) REFERENCES afk(id)
+            );
+            ",
+        kind: MigrationKind::Up,
+    }];
+
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+            let window = app.get_window("main").unwrap();
+            window.open_devtools();
+            window.close_devtools();
+            }
+            Ok(())
+        })
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                // macos location: ~/~/Library/Application\ Support/<tauri.bundle.identifier>/<dbname>.db
+                .add_migrations("sqlite:afk-track.db", migrations)
+                .build(),
+        )
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,130 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
+import { register, unregisterAll } from '@tauri-apps/api/globalShortcut';
+import Database from "tauri-plugin-sql-api";
 import "./App.css";
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+// sqlite. The path is relative to `tauri::api::path::BaseDirectory::App`.
+const db = await Database.load("sqlite:afk-track.db");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+// Away from Keyboard
+type Afk = {
+  id: number,
+  from: Date,
+  to?: Date,
+}
+
+const totalMin = (afk: Afk): number | undefined => {
+  if (!afk.to) {
+    return undefined
   }
+  return Math.floor((afk.to.getTime() - afk.from.getTime()) / (1000 * 60))
+}
+
+const ymdStr = (offsetDay: number = 0) : string => {
+    const now = new Date(Date.now());
+    now.setDate(now.getDate() + offsetDay)
+    const yyyy = now.getFullYear();
+    const mm = (now.getMonth() + 1).toString().padStart(2, '0');
+    const dd = now.getDate().toString().padStart(2, '0');
+    return `${yyyy}-${mm}-${dd}`;
+  }
+
+function App() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [isAfk, setIsAfk] = useState<boolean>(false);
+  const [afks, setAfks] = useState<Afk[]>([]);
+  const currentId = useRef<number>(0);
+
+  useEffect(() => {
+    if (loading) {
+      return;
+    }
+    (async () => {
+      const res = await currentAfks();
+      setAfks(res);
+      currentId.current = (Math.max(...res.map(item => item.id)));
+    })();
+    setLoading(true);
+  }, [loading]);
+
+  useEffect(() => {
+    (async () => {
+      await unregisterAll();
+      await register('CommandOrControl+Shift+C', async () => {
+        if (isAfk) {
+          await bak();
+        } else {
+          await afk();
+        };
+      });
+      const res = await currentAfks();
+      setAfks(res);
+      currentId.current = (Math.max(...res.map(item => item.id)));
+    })();
+  }, [isAfk])
+
+  const currentAfks = async () => {
+    type Table = {
+      id: number,
+      afk_at: string,
+      bak_at?: string,
+    }
+    const data: Table[] = await db.select(`
+      SELECT 
+        a.id, 
+        datetime(a.created_at, 'localtime') as afk_at, 
+        datetime(b.created_at, 'localtime') as bak_at
+      FROM afk as a
+      LEFT JOIN bak as b
+      ON a.id = b.afk_id
+      WHERE datetime(a.created_at, 'localtime') between '${ymdStr()}' and '${ymdStr(1)}'
+    `);
+    const res = data.map((item) => ({
+      id: item.id,
+      from: new Date(item.afk_at),
+      to: item.bak_at ? new Date(item.bak_at) : undefined,
+    }));
+    return res;
+  }
+  
+  // away from keyboard
+  const afk = async () => {
+    await db.execute("INSERT into afk DEFAULT VALUES");
+    setIsAfk((current) => !current);
+  };
+
+  // back at keyboard
+  const bak = async () => {
+    await db.execute( "INSERT into bak (afk_id) VALUES ($1)", [currentId.current]);
+    setIsAfk((current) => !current);
+  };
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
+      <p>{isAfk ? "離席中" : "在席中"}</p>
+      <p>{currentId.current}</p>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>From</th>
+            <th>To</th>
+            <th>Minutes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {afks.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.from.toLocaleString()}</td>
+              <td>{item.to?.toLocaleString()}</td>
+              <td>{totalMin(item)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
